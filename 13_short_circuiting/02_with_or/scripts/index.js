df30651b97fbd0033af97861f0adb30ff92fb300
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2026-09-03 17:02:18 +0300

    adding comments and fix typo

--- 13_short_circuiting/02_with_or/scripts/index.js
+++ 13_short_circuiting/02_with_or/scripts/index.js
@@ -1,5 +1,7 @@
 let price = undefined;
 
-undefined || console.log(`price is undefined`);
+// The following will execute the right side expression even if price is undefined, null, false, 0, NaN, or ""
+price || console.log(`price is ${price}`);
 
+// The following will print the price if it's not undefined, null, false, 0, NaN, or "", otherwise it will print 'empty'
 console.warn(`price is ${price || 'empty'}`);
